fd52fa97010b1cb460a60e8c64f2c5c68771c2ad
Anamika T A <anamikata07@gmail.com>
2026-09-20 12:12:15 +0530

    Changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/SkillTrace-AI/',
   plugins: [react()],
+})
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
